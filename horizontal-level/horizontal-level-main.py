9b645512d63726df93c37e31242eacfef117a232
Piero Cornice <piero.cornice@intelligentlilli.com>
2023-08-09 08:43:19 +0100

    horizontal-level: bugfix

--- horizontal-level/horizontal-level-main.py
+++ horizontal-level/horizontal-level-main.py
@@ -10,7 +10,7 @@ while True:
     
     x_is_level = -tolerance <= x_strength <= tolerance
     y_is_level = -tolerance <= y_strength <= tolerance
-    z_is_level = -1024 - tolerance <= z_strength <= 1024 + tolerance
+    z_is_level = (-1024 - tolerance) <= z_strength <= (-1024 + tolerance)
     
     if x_is_level and y_is_level and z_is_level:
         display.show(Image.HAPPY)
