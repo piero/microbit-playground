5b8f62f05b8f6fd7ab17b8ac47be124d98e25fb7
Piero Cornice <piero.cornice@intelligentlilli.com>
2023-08-09 14:44:26 +0100

    level-game: Add score and animation

--- level-game/level-game-main.py
+++ level-game/level-game-main.py
@@ -6,14 +6,31 @@ def normalize(value, input_range, output_range) -> float:
     return output_range[0] + ((output_range[1] - output_range[0]) / (input_range[0] - input_range[1])) * (value - input_range[0])
 
 
-# Set the initial target
+def animate_win():
+    display.show(Image('00000:00600:06960:00600:00000'))
+    sleep(50)
+    display.show(Image('00600:06960:69696:06960:00600'))
+    sleep(50)
+    display.show(Image('06960:69696:96069:69696:06960'))
+    sleep(50)
+    display.show(Image('69696:96069:60006:96069:69696'))
+    sleep(50)
+    display.show(Image('96069:60006:00000:60006:96069'))
+    sleep(50)
+    display.show(Image('60006:00000:00000:00000:60006'))
+    sleep(50)
+    display.clear()
+
+
+
+# Set the initial target and initialise score
 x_target = randint(0, 4)
 y_target = randint(0, 4)
+score = 0
 
 
 while True:    
-    x_acc = accelerometer.get_x()
-    y_acc = accelerometer.get_y()
+    x_acc, y_acc, _ = accelerometer.get_values()
 
     x_acc = min(max(x_acc, -500), 500)
     y_acc = min(max(y_acc, -500), 500)
@@ -26,10 +43,18 @@ while True:
 
     if x_pos == x_target and y_pos == y_target:
         # Win! Set a new target
-        display.show(Image.TARGET)
+        score += 1
+        display.show(score)
         sleep(1000)
-        x_target = randint(0, 4)
-        y_target = randint(0, 4)
+        if score < 9:
+            animate_win()
+            x_target = randint(0, 4)
+            y_target = randint(0, 4)
+        else:
+            # Restart the game
+            for _ in range(3):
+                animate_win()
+            score = 0
     
     display.clear()
     display.set_pixel(x_target, y_target, 5)
