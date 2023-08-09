988b1bbaa1a46c4a1ea31868f39719a8987af17f
Piero Cornice <piero.cornice@intelligentlilli.com>
2023-08-09 08:44:34 +0100

    horizontal-level: optimization

--- horizontal-level/horizontal-level-main.py
+++ horizontal-level/horizontal-level-main.py
@@ -4,13 +4,9 @@ from micropython import const
 tolerance = const(100)
 
 while True:
-    x_strength = accelerometer.get_x()
-    y_strength = accelerometer.get_y()
-    z_strength = accelerometer.get_z()
-    
-    x_is_level = -tolerance <= x_strength <= tolerance
-    y_is_level = -tolerance <= y_strength <= tolerance
-    z_is_level = (-1024 - tolerance) <= z_strength <= (-1024 + tolerance)
+    x_is_level = -tolerance <= accelerometer.get_x() <= tolerance
+    y_is_level = -tolerance <= accelerometer.get_y() <= tolerance
+    z_is_level = (-1024 - tolerance) <= accelerometer.get_z() <= (-1024 + tolerance)
     
     if x_is_level and y_is_level and z_is_level:
         display.show(Image.HAPPY)
